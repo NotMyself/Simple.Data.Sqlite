94cb1f4c043e853aa2971761560ed358e9b18a6f
Bobby Johnson <bobby.johnson@gmail.com>
2011-06-17 09:23:33 -0700

    setting up basic project

---
+++ src/Simple.Data.SqliteTests/RealityTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Simple.Data.SqliteTests
+{
+    [TestFixture]
+    public class RealityTests
+    {
+        [Test]
+        public void true_should_be_true()
+        {
+            Assert.That(true,Is.True);
+        }
+    }
+}
