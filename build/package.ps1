8b13125407f1c0bb90ef1ac9751a3c64fe750c7b
Bobby Johnson <bobby.johnson@gmail.com>
2011-07-29 12:51:06 -0700

    adding switch to produce symbol package

--- build/package.ps1
+++ build/package.ps1
@@ -5,7 +5,7 @@ properties {
 
 task package -depends test {
 	Push-Location "$source_dir\Simple.Data.Sqlite"
-	& $nuget pack .\Simple.Data.Sqlite.csproj -Properties Configuration=Release -Version $version -OutputDirectory $nuget_pub_dir
+	& $nuget pack .\Simple.Data.Sqlite.csproj -Sym -Properties Configuration=Release -Version $version -OutputDirectory $nuget_pub_dir
 	Pop-Location
 }
 
