b3577b8e3571fe4d20dd215cc1ce113773598815
Bobby Johnson <bobby.johnson@gmail.com>
2011-08-20 12:14:46 -0700

    added some additional missing tests from the SQLCE project

---
+++ src/Simple.Data.SqliteTests/QueryTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+
+namespace Simple.Data.SqliteTests
+{
+    //TODO: add a table to the northwind db to make these tests pass.
+    [TestFixture]
+    [Ignore]
+    public class QueryTests
+    {
+        private static readonly string DatabasePath = Path.Combine(
+           Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase.Substring(8)),
+           "Northwind.db");
+
+        [Test]
+        public void ShouldSelectFromOneToTen()
+        {
+            var db = Database.Opener.OpenFile(DatabasePath);
+            var query = db.PagingTest.QueryById(1.to(100)).Take(10);
+            int index = 1;
+            foreach (var row in query)
+            {
+                Assert.AreEqual(index, row.Id);
+                index++;
+            }
+        }
+
+        [Test]
+        public void ShouldSelectFromElevenToTwenty()
+        {
+            var db = Database.Opener.OpenFile(DatabasePath);
+            var query = db.PagingTest.QueryById(1.to(100)).Skip(10).Take(10);
+            int index = 11;
+            foreach (var row in query)
+            {
+                Assert.AreEqual(index, row.Id);
+                index++;
+            }
+        }
+
+        [Test]
+        public void ShouldSelectFromOneHundredToNinetyOne()
+        {
+            var db = Database.Opener.OpenFile(DatabasePath);
+            var query = db.PagingTest.QueryById(1.to(100)).OrderByDescending(db.PagingTest.Id).Skip(0).Take(10);
+            int index = 100;
+            foreach (var row in query)
+            {
+                Assert.AreEqual(index, row.Id);
+                index--;
+            }
+        }
+    }
+}
---
+++ src/Simple.Data.SqliteTests/SchemaProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Simple.Data.Sqlite;
+
+namespace Simple.Data.SqliteTests
+{
+    [TestFixture]
+    public class SchemaProviderTest
+    {
+        [Test]
+        public void NullConnectionProviderCausesException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SqliteSchemaProvider(null));
+        }
+
+        [Test]
+        public void ProceduresIsEmpty()
+        {
+            Assert.AreEqual(0, new SqliteSchemaProvider(new SqliteConnectionProvider()).GetStoredProcedures().Count());
+        }
+    }
+}
