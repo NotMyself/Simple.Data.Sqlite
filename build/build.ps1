f72d279ece6c0c856bffb36651905ff700309a1c
Bobby Johnson <bobby.johnson@gmail.com>
2011-08-20 10:29:47 -0700

    updating to version 0.9.1.1 of the simple data libraries

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,5 @@
 properties {
-	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.8.2.1" }
+	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.1.1" }
 }
 
 include .\master_build.ps1
