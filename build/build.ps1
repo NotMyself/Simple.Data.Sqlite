a85c72fa71b4954aa62bdda49cbf7fef5380451e
NotMyself <bobby.johnson@gmail.com>
2011-10-24 15:12:14 -0700

    updating to simple.data 9.8.2

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,5 @@
 properties {
-	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.6.3" }
+	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.8.2" }
 }
 
 include .\master_build.ps1
