440b428c0feb27f57ba7231d5b56fdeb082217a7
NotMyself <bobby.johnson@gmail.com>
2011-11-04 11:59:10 -0700

    updated nuget so that symbol server publishing will work agian.

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,5 @@
 properties {
-	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.8.2" }
+	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.8.3" }
 }
 
 include .\master_build.ps1
