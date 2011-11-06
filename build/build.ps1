5be0da1180742a5e38019eb5235ef22f70e5b4b7
NotMyself <bobby.johnson@gmail.com>
2011-11-06 13:09:44 -0800

    updating to 0.9.8.5

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,5 @@
 properties {
-	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.8.3" }
+	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.8.5" }
 }
 
 include .\master_build.ps1
