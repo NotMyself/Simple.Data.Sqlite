8656357dc9033fbd52c1aee772bd28a98be016ab
NotMyself <bobby.johnson@gmail.com>
2011-11-25 08:40:34 -0800

    updating to 0.11.2.1

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,5 @@
 properties {
-	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.9.8.5" }
+	$version = if($env:BUILD_NUMBER) {$env:BUILD_NUMBER} else { "0.11.2.1" }
 }
 
 include .\master_build.ps1
