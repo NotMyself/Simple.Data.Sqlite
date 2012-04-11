c6f0c627c59df6dab14e0f2baf7e3893bf56571e
vidarls <vidar.l.somme@gmail.com>
2012-04-11 11:41:53 +0200

    Updated to work with Simple.Data 0.16.0.0
    Allso implemented new method on interface IQueryPager as required by
    Simple.Data 0.16.0.0

--- src/Simple.Data.Sqlite/SqliteQueryPager.cs
+++ src/Simple.Data.Sqlite/SqliteQueryPager.cs
@@ -12,7 +12,20 @@ namespace Simple.Data.Sqlite
     {
         private static readonly Regex ColumnExtract = new Regex(@"SELECT\s*(.*)\s*(FROM.*)", RegexOptions.Multiline | RegexOptions.IgnoreCase);
 
+        public IEnumerable<string> ApplyLimit(string sql, int take)
+        {
+            sql = AddMissingOrderBy(sql);
+            yield return string.Format("{0} LIMIT {1}", sql, take);
+        }
+
         public IEnumerable<string> ApplyPaging(string sql, int skip, int take)
+        {
+            sql = AddMissingOrderBy(sql);
+
+            yield return string.Format("{0} LIMIT {1},{2}", sql, skip, take);
+        }
+
+        private string AddMissingOrderBy(string sql)
         {
             if (sql.IndexOf("order by", StringComparison.InvariantCultureIgnoreCase) < 0)
             {
@@ -20,8 +33,7 @@ namespace Simple.Data.Sqlite
                 var columns = match.Groups[1].Value.Trim();
                 sql += " ORDER BY " + columns.Split(',').First().Trim();
             }
-
-            yield return string.Format("{0} LIMIT {1},{2}", sql, skip, take);
+            return sql;
         }
     }
 }
